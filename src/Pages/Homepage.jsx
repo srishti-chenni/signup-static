eb9855ce8b5aef5badb62fb854401415fe3d3e0f
srishti-chenni <Srishti.Chenni@genpact.com>
2023-06-04 18:45:39 +0530

    inital commit

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,207 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Select from "react-select";
+
+const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [interests, setInterests] = useState([]);
+  const [bio, setBio] = useState("");
+  const [photos, setPhotos] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "firstName") {
+      setFirstName(value);
+    } else if (name === "lastName") {
+      setLastName(value);
+    } else if (name === "dob") {
+      setDob(value);
+    } else if (name === "gender") {
+      setGender(value);
+    } else if (name === "phone") {
+      setPhone(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "bio") {
+      setBio(value);
+    }
+  };
+
+  const handleInterestsChange = (selectedOptions) => {
+    setInterests(selectedOptions);
+  };
+
+  const handlePhotoChange = (e) => {
+    const files = Array.from(e.target.files);
+    setPhotos(files);
+  };
+
+  const handleSubmit = () => {
+    // Perform submit logic here
+    console.log("Submitted data");
+    // Add logic to store data in the database
+  };
+
+  const interestsOptions = [
+    { value: "rafting", label: "Rafting" },
+    { value: "mountain", label: "Mountain Climbing" },
+    { value: "bar", label: "Bar Hopping" },
+    { value: "city", label: "City Tour" },
+  ];
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2>Signup</h2>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="firstName" className="form-label">
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="firstName"
+                    name="firstName"
+                    placeholder="Enter first name"
+                    value={firstName}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="lastName" className="form-label">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Enter last name"
+                    value={lastName}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="dob" className="form-label">
+                    Date of Birth
+                  </label>
+                  <input
+                    type="date"
+                    className="form-control"
+                    id="dob"
+                    name="dob"
+                    value={dob}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="gender" className="form-label">
+                    Gender
+                  </label>
+                  <select
+                    className="form-select"
+                    id="gender"
+                    name="gender"
+                    value={gender}
+                    onChange={handleInputChange}
+                  >
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="phone" className="form-label">
+                    Phone
+                  </label>
+                  <input
+                    type="tel"
+                    className="form-control"
+                    id="phone"
+                    name="phone"
+                    placeholder="Enter phone number"
+                    value={phone}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="interests" className="form-label">
+                    Interests
+                  </label>
+                  <Select
+                    id="interests"
+                    name="interests"
+                    isMulti
+                    options={interestsOptions}
+                    value={interests}
+                    onChange={handleInterestsChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="bio" className="form-label">
+                    Bio
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="bio"
+                    name="bio"
+                    rows="3"
+                    placeholder="Enter your bio"
+                    value={bio}
+                    onChange={handleInputChange}
+                  ></textarea>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="photos" className="form-label">
+                    Photos (Upload two)
+                  </label>
+                  <input
+                    type="file"
+                    className="form-control"
+                    id="photos"
+                    name="photos"
+                    multiple
+                    accept="image/*"
+                    onChange={handlePhotoChange}
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
